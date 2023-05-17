7ac9f8efb578c4a588465482b0576d84a8b23679
Harrison <harrison.campbell0101@gmail.com>
2023-05-18 00:16:14 +0100

    Updated header info

--- zMute.user.js
+++ zMute.user.js
@@ -5,8 +5,8 @@
 // @grant		GM_getValue
 // @grant		GM_setValue
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @description  I hate Sinz - This script lets you mute him.
+// @author       hcampbell.dev
 // @match        https://zarpgaming.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=zarpgaming.com
 // ==/UserScript==
