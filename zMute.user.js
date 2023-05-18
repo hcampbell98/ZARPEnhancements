c7faf5aa3560e59151587f9fc007b6f38b976ed4
Harrison <harrison.campbell0101@gmail.com>
2023-05-18 15:55:58 +0100

    Increment version number

--- zMute.user.js
+++ zMute.user.js
@@ -4,7 +4,7 @@
 // @require      zUser.js
 // @grant        GM_getValue
 // @grant        GM_setValue
-// @version      0.4
+// @version      0.5
 // @description  I hate Sinz - This script lets you mute him.
 // @author       hcampbell.dev
 // @match        https://zarpgaming.com/*
