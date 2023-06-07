3bf2303d09f7ecdd4cc81d050ad9474af772e9d6
Harrison <harrison.campbell0101@gmail.com>
2023-06-07 18:42:25 +0100

    Refactored code

--- zUser.js
+++ zUser.js
@@ -1,6 +1,7 @@
 class User {
-    constructor(profileUrl, username = "") {
-        this.profileUrl = this.getProfileId(profileUrl);
+    constructor(profileUrl, username = "", muted = false, interactedWith = false) {
+        this.profileUrl = profileUrl;
+        this.profileId = this.getProfileId(profileUrl);
 
         // If username is not provided, we'll extract it from the profileUrl
         // the profileUrl is in the format: https://zarpgaming.com/index.php/forum/profile/12345-username
@@ -9,9 +10,8 @@ class User {
             this.username = profileUrl.match(usernameRegex)[1];
         } else this.username = username;
 
-        this.muted = this.isMuted();
-
-        this.interactedWith = this.isInteractedWith();
+        this.muted = muted;
+        this.interactedWith = interactedWith;
     }
 
     getProfileId(profileUrl) {
@@ -22,49 +22,55 @@ class User {
         return this.username;
     }
 
-    toggleMute() {
-        this.interactedWith = true;
-        let interactedUsers = GM_getValue("interactedUsers", []);
-        if (!interactedUsers.includes(this.profileUrl)) {
-            interactedUsers.push(this.profileUrl);
-            GM_setValue("interactedUsers", interactedUsers);
-        }
-
-        if (this.muted) this.unmute();
-        else this.mute();
-    }
-
     isMuted() {
-        let mutedUsers = GM_getValue("mutedUsers", []);
-        return mutedUsers.includes(this.profileUrl);
+        let storedUsers = GM_getValue("storedUsers", []);
+
+        for (let i = 0; i < storedUsers.length; i++) {
+            if (storedUsers[i].profileId === this.profileId) {
+                return storedUsers[i].muted;
+            }
+        }
     }
 
     isInteractedWith() {
         let interactedUsers = GM_getValue("interactedUsers", []);
-        return interactedUsers.includes(this.profileUrl);
+        return interactedUsers.includes(this.profileId);
+    }
+
+    update() {
+        this.interactedWith = true;
+        //We want to update the stored user
+        let storedUsers = GM_getValue("storedUsers", []);
+
+        //If the user is already stored, we want to update their values
+        //If the user is not stored, we want to add them to the stored users
+        let userStored = false;
+        for (let i = 0; i < storedUsers.length; i++) {
+            if (storedUsers[i].profileId === this.profileId) {
+                storedUsers[i].muted = this.muted;
+                storedUsers[i].interactedWith = this.interactedWith;
+                userStored = true;
+            }
+        }
+
+        if (!userStored) storedUsers.push(this);
+
+        GM_setValue("storedUsers", storedUsers);
     }
 
     mute() {
         this.muted = true;
-        let mutedUsers = GM_getValue("mutedUsers", []);
-
-        if (!mutedUsers.includes(this.profileUrl)) {
-            mutedUsers.push(this.profileUrl);
-            GM_setValue("mutedUsers", mutedUsers);
-            console.log(GM_getValue("mutedUsers", []));
-        }
-
-        window.location.reload();
+        this.update();
     }
 
     unmute() {
         this.muted = false;
-        let mutedUsers = GM_getValue("mutedUsers", []);
+        this.update();
+    }
 
-        if (mutedUsers.includes(this.profileUrl)) {
-            mutedUsers.splice(mutedUsers.indexOf(this.profileUrl), 1);
-            GM_setValue("mutedUsers", mutedUsers);
-        }
+    toggleMute() {
+        if (this.isMuted()) this.unmute();
+        else this.mute();
 
         window.location.reload();
     }
