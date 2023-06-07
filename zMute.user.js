c42d69ab3105974d13d7809819fad65e29c0c8ba
Harrison <harrison.campbell0101@gmail.com>
2023-06-07 18:55:43 +0100

    Added removal of quotes from muted users

--- zMute.user.js
+++ zMute.user.js
@@ -4,7 +4,7 @@
 // @require      zUser.js
 // @grant        GM_getValue
 // @grant        GM_setValue
-// @version      1.3
+// @version      1.4
 // @description  I hate Sinz - This script lets you mute him.
 // @author       hcampbell.dev
 // @match        https://zarpgaming.com/*
@@ -21,7 +21,7 @@
         currentUser.logUser();
     }
 
-    var defaultMutedUsers = [new User("https://zarpgaming.com/index.php/forum/profile/37312-sinzz")];
+    var defaultMutedUsers = [new User("https://zarpgaming.com/index.php/forum/profile/37312-sinzz", "sinzz")];
 
     //Mute users in defaultMutedUsers if they aren't already muted
     defaultMutedUsers.forEach((user) => {
@@ -60,13 +60,14 @@
     }
     //If we're on a thread, we want to hide posts from muted users
     else if (window.location.href.includes("/forum/")) {
-        let mutedUsers = GM_getValue("mutedUsers", []);
+        let storedUsers = GM_getValue("storedUsers", []);
         let postsOnPage = document.querySelectorAll(".kmsg");
 
         postsOnPage.forEach((post) => {
             let profileUrl = "https://zarpgaming.com" + post.querySelector(".kpost-username>a").getAttribute("href");
+            let user = new User(profileUrl);
 
-            if (mutedUsers.includes(profileUrl)) {
+            if (user.isMuted()) {
                 // We want to add a click event handler to the message header, that will toggle the message body
                 // The message header is the element directly above the message body
                 let messageHeader = post.previousElementSibling;
@@ -83,12 +84,53 @@
                 // Hide message body
                 post.setAttribute("hidden", "");
             }
+
+            //This removes quotes from muted users
+            let quotes = post.querySelectorAll(".kmsgtext-quote");
+            quotes.forEach((quote) => {
+                //Get the element directly above the quote
+                let quoteHeader = quote.previousElementSibling;
+
+                if (!quoteHeader) return;
+
+                //The quote header is in the format: username wrote:
+                //We want to get the username from the quote header
+                //Remove last 7 characters from quote header to get username
+                let quoteUsername = quoteHeader.innerText.slice(0, -7);
+
+                //We want to check if the quote is from a muted user
+                for (let i = 0; i < storedUsers.length; i++) {
+                    let mutedUser = new User(storedUsers[i].profileUrl);
+                    if (mutedUser.getUsername() === quoteUsername) {
+                        let offset = 0;
+
+                        //For each child of the quote header, check if it has the 'kmsgtext-quote' class
+                        //If it does, set the offset to the index of the child and return
+                        for (let i = 0; i < quote.children.length; i++) {
+                            if (quote.children[i].classList.contains("kmsgtext-quote")) {
+                                offset = 3;
+                                break;
+                            }
+                        }
+
+                        //Remove every child of the quote element, starting from the offset
+                        while (quote.childNodes.length > offset) {
+                            quote.removeChild(quote.lastChild);
+                        }
+
+                        //Add text saying "Quote from muted user"
+                        let mutedQuote = document.createTextNode("This message has been removed because it is a quoted message from a muted user.");
+                        quote.appendChild(mutedQuote);
+
+                        //Replace the quote header with a new one saying "Muted User wrote:"
+                        quoteHeader.innerText = "Muted User wrote:";
+                    }
+                }
+            });
         });
     }
     //If we're on the forum page, we want to hide threads from muted users
     else {
-        let mutedUsers = GM_getValue("mutedUsers", []);
-
         //The elements we want are the parent of the parent of the .ktopic-details element
         //Use a map to get the parent of the parent of each .ktopic-details element
 
@@ -99,9 +141,11 @@
         //Changes "Last Post by" to "Last Post by Muted User" if the last post was by a muted user
         postsOnPage.forEach((post) => {
             let profileUrl = "https://zarpgaming.com" + post.querySelector(".ktopic-latest-post > a:nth-child(2)").getAttribute("href");
+            let user = new User(profileUrl);
+
             let lastPostBy = post.querySelector(".ktopic-latest-post > a:nth-child(2)");
 
-            if (mutedUsers.includes(profileUrl)) {
+            if (user.isMuted()) {
                 lastPostBy.innerText = "Muted User";
 
                 // Replace avatar for muted user
@@ -114,8 +158,9 @@
         postsOnPage.forEach((post) => {
             let profileUrl = "https://zarpgaming.com" + post.querySelector(".ktopic-by > a").getAttribute("href");
             let postHref = post.querySelector(".ktopic-title-cover>a").getAttribute("href");
+            let user = new User(profileUrl);
 
-            if (mutedUsers.includes(profileUrl))
+            if (user.isMuted())
                 post.innerHTML = `<td class='kcol-first'></td><td class='kcol-mid'></td><td class='kcol-mid kcol-ktopictitle'><div class='ktopic-title-cover'><a href='${postHref}'>This user has been muted</a></div></td><td class='kcol-mid'></td><td class='kcol-mid'></td>`;
         });
     }
